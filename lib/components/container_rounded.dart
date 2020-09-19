7441a8b56e660b4d978e7146b1e6946bce664ce4
Nayeem Islam <47152362+NayeemCseMu@users.noreply.github.com>
2020-09-19 18:45:54 +0600

    added childwidget

--- lib/components/container_rounded.dart
+++ lib/components/container_rounded.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import '../constants.dart';
 import '../size.dart';
 
-class TextFieldContainer extends StatelessWidget {
-  TextFieldContainer({this.childWidget});
+class RoundContainer extends StatelessWidget {
+  RoundContainer({this.childWidget});
   final Widget childWidget;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      alignment: Alignment.center,
       height: getScreenHeight(55),
       width: getScreeWidth(335),
       padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
