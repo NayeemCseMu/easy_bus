fbcea8da2a0ed3db57d3991fcd84ef5b5aa0d356
Nayeem Islam <47152362+NayeemCseMu@users.noreply.github.com>
2020-09-20 14:08:28 +0600

    wrap root widget with provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:easy_bus/register/register_screen.dart';
 import 'package:easy_bus/size.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'components/tab_item.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,21 +16,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of the EasyBus application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'EasyBuss',
-        theme: ThemeData(
-          scaffoldBackgroundColor: kBackgroundColor,
-          textTheme:
-              GoogleFonts.montserratTextTheme(Theme.of(context).textTheme),
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: SplashScreen(),
-        routes: {
-          SplashScreen.login_screen: (context) => LoginScreen(),
-          RegisterScreen.register_screen: (context) => RegisterScreen(),
-          SearchScreen.search_screen: (context) => SearchScreen(),
-        });
+    return ChangeNotifierProvider(
+      create: (context) => HomeTab(),
+      child: MaterialApp(
+          title: 'EasyBuss',
+          theme: ThemeData(
+            scaffoldBackgroundColor: kBackgroundColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(Theme.of(context).textTheme),
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: SplashScreen(),
+          routes: {
+            SplashScreen.login_screen: (context) => LoginScreen(),
+            RegisterScreen.register_screen: (context) => RegisterScreen(),
+            SearchScreen.search_screen: (context) => SearchScreen(),
+          }),
+    );
   }
 }
 
