217a09426ac20197bf7f9fc4d9f8e132820dc9c9
Nayeem Islam <47152362+NayeemCseMu@users.noreply.github.com>
2020-09-22 00:29:14 +0600

    search screen added

---
+++ lib/search/search_ticket.dart
@@ -0,0 +1,15 @@
+import 'package:easy_bus/components/app_bar.dart';
+import 'package:easy_bus/search/components/body.dart';
+import 'package:flutter/material.dart';
+
+class SearchTicket extends StatelessWidget {
+  static const seach_ticket = "search_ticket";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: buildAppBar(title: 'Sylhet to Dhaka'),
+      body: Body(),
+    );
+  }
+}
