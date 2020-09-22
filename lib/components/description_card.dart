fec5231be5137c9fb52b9f7127c2253bce415789
Nayeem Islam <47152362+NayeemCseMu@users.noreply.github.com>
2020-09-22 21:47:20 +0600

    removed aspectratio

--- lib/components/description_card.dart
+++ lib/components/description_card.dart
@@ -9,23 +9,14 @@ class DescriptionCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: kDefaultPadding, vertical: kDefaultPadding),
-      child: AspectRatio(
-        aspectRatio: largeCard ? 1.45 : 1.9,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-              boxShadow: [kCardBoxShadow]),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: kDefaultPadding / 2, vertical: kDefaultPadding),
-            child: cardChild,
-          ),
-        ),
-      ),
+    return Container(
+      padding: EdgeInsets.all(kDefaultPadding),
+      margin: EdgeInsets.all(kDefaultPadding),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [kCardBoxShadow]),
+      child: cardChild,
     );
   }
 }
