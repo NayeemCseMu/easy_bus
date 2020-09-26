74532ee7dbceb042210966997ec9c8464084d2f3
Nayeem Islam <47152362+NayeemCseMu@users.noreply.github.com>
2020-09-26 19:24:33 +0600

    changes in color and add trailing

--- lib/components/app_bar.dart
+++ lib/components/app_bar.dart
@@ -2,23 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:easy_bus/constants.dart';
 
-AppBar buildAppBar({String title}) {
+AppBar buildAppBar(
+    {String title, Color color = Colors.white, bool trailing = true}) {
   return AppBar(
     elevation: 0,
     backgroundColor: Colors.transparent,
     leading: IconButton(
       onPressed: () {},
-      icon: SvgPicture.asset('assets/icons/back_arrow.svg'),
+      icon: SvgPicture.asset('assets/icons/back_arrow.svg', color: color),
     ),
     title: Text(
       title,
-      style: kSemiBoldText.copyWith(color: Colors.white),
+      style: kBoldText.copyWith(
+        color: Colors.white,
+      ),
     ),
     centerTitle: true,
     actions: [
       Padding(
         padding: const EdgeInsets.only(right: kDefaultPadding),
-        child: SvgPicture.asset('assets/icons/menu.svg'),
+        child:
+            trailing ? SvgPicture.asset('assets/icons/menu.svg') : SizedBox(),
       ),
     ],
   );
