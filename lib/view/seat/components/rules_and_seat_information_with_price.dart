f7bea68cce8b6f2cc86e2b11d6b1fa8034aaa959
Nayeem Islam <47152362+NayeemCseMu@users.noreply.github.com>
2020-09-26 10:08:27 +0600

    refactor from body

---
+++ lib/view/seat/components/rules_and_seat_information_with_price.dart
@@ -0,0 +1,102 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:easy_bus/model/seats_icon.dart';
+import 'package:easy_bus/view/home/components/tab_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../constants.dart';
+import '../../../size.dart';
+
+class RulesAndSeatInformationWithPrice extends StatelessWidget {
+  const RulesAndSeatInformationWithPrice({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: getScreeWidth(187),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ...List.generate(
+                textAndIcon.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: TextWidget(
+                    icon: textAndIcon[index].icon,
+                    text: textAndIcon[index].text,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        buildText(text: 'Your Seats'),
+        SizedBox(
+          width: getScreeWidth(187),
+          child: Row(
+            children: <Widget>[
+              ...List.generate(
+                seatSerial.length,
+                (index) => Padding(
+                  padding: const EdgeInsets.only(
+                      right: kDefaultPadding / 2, top: kDefaultPadding / 2),
+                  //here we used TabItem button style for display our selected seat.
+                  child: TabItem(
+                    title: seatSerial[index],
+                    isIcon: false,
+                    isActive: true,
+                    press: () {},
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        buildText(text: 'Class'),
+        buildText(text: 'Economy(AC)', color: kTextColor),
+        buildText(text: 'Total Cost'),
+        buildText(text: '\$160 (4 persons)', color: kTextColor),
+      ],
+    );
+  }
+
+  Padding buildText({String text, Color color = kTextGreenColor}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: kDefaultPadding / 1.5),
+      child: Text(
+        text,
+        style: kBoldText.copyWith(color: color),
+      ),
+    );
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  final String icon, text;
+
+  TextWidget({this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SvgPicture.asset(icon),
+        SizedBox(width: 10),
+        SizedBox(
+          width: getScreeWidth(158),
+          child: AutoSizeText(
+            text,
+            maxFontSize: 12,
+            maxLines: 2,
+            style: kMediumText.copyWith(fontSize: 12),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/view/seat/components/seat_icon_visual_card.dart
@@ -0,0 +1,42 @@
+import 'package:easy_bus/model/tab_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+
+import '../../../constants.dart';
+import '../../../size.dart';
+
+class SeatIconVisualForSelection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: getScreeWidth(125),
+      height: getScreenHeight(386),
+      child: GridView.builder(
+        scrollDirection: Axis.horizontal,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 10,
+          childAspectRatio: 1.25,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 5,
+        ),
+        itemCount: 40,
+        itemBuilder: (context, index) {
+          final provider = Provider.of<HomeTab>(context);
+          return GestureDetector(
+              child: SvgPicture.asset(
+                'assets/icons/available_seat.svg',
+                color: provider.seatSelectIndex == index
+                    ? kPrimaryColor
+                    : kIconColor,
+                height: getScreeWidth(24),
+                width: getScreeWidth(20),
+              ),
+              onTap: () {
+                provider.getSeatIndex(index);
+              });
+        },
+      ),
+    );
+  }
+}
