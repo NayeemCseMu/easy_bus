56f1e3efa48d5a0a7d883d24a1ed620143442700
NayeemCseMu <nayeem.cse1@gmail.com>
2020-11-20 16:53:09 +0600

    customized usable button

---
+++ lib/components/button.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../size.dart';
+
+class Button extends CustomRaisedButton {
+  final String title;
+  final Color color;
+  final double fontSize;
+  final Function press;
+  Button({
+    @required this.title,
+    this.color,
+    this.fontSize,
+    this.press,
+  }) : super(
+          buttonChild: Text(
+            title.toUpperCase(),
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: fontSize,
+                fontWeight: FontWeight.w600),
+          ),
+          color: color,
+          fontSize: fontSize,
+          press: press,
+        );
+}
+
+class ButtonIcon extends CustomRaisedButton {
+  final String title;
+  final String icon;
+  final Color color;
+  final double fontSize;
+  final Function press;
+  ButtonIcon({
+    @required this.title,
+    @required this.icon,
+    this.color,
+    this.fontSize,
+    this.press,
+  }) : super(
+          buttonChild: Row(
+            children: [
+              SizedBox(width: 10),
+              SvgPicture.asset(icon),
+              SizedBox(width: 10),
+              Text(
+                title.toUpperCase(),
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          color: color,
+          fontSize: fontSize,
+          press: press,
+        );
+}
+
+class CustomRaisedButton extends StatelessWidget {
+  const CustomRaisedButton({
+    this.color,
+    this.press,
+    this.fontSize,
+    this.buttonChild,
+  });
+
+  final Color color;
+  final Function press;
+  final double fontSize;
+  final Widget buttonChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: getScreenHeight(55),
+      width: getScreeWidth(335),
+      child: RaisedButton(
+          elevation: 1.0,
+          color: color,
+          onPressed: press,
+          shape: buildRoundedRectangleBorder(),
+          child: buttonChild),
+    );
+  }
+
+  RoundedRectangleBorder buildRoundedRectangleBorder() {
+    return RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(getScreenHeight(27.5)));
+  }
+}
