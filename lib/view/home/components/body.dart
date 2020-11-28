ead6b3066c28e17d1f7c37c38c21572c65862367
NayeemCseMu <nayeem.cse1@gmail.com>
2020-11-28 10:51:40 +0600

    scroll and card

--- lib/view/home/components/body.dart
+++ lib/view/home/components/body.dart
@@ -1,11 +1,10 @@
+import 'package:easy_bus/model/history.dart';
 import 'package:flutter/material.dart';
 import "package:flutter/cupertino.dart";
-import 'package:provider/provider.dart';
-import '../../../components/history_and_search_content.dart';
 import '../../../components/history_and_search_card.dart';
+import '../../../components/history_and_search_content.dart';
 import '../../../components/custom_dropdown.dart';
 import '../../../components/header.dart';
-import '../../../model/tab_item.dart';
 import '../../../utilis/constants.dart';
 import '../../../utilis/size.dart';
 
@@ -17,47 +16,52 @@ enum button { one, round }
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<HomeTab>(
-      builder: (context, tabItem, child) => Column(
-        children: [
-          Header(
-            headerChild: CustomDropDown(),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  getVerticalSpace(15),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: TabButton(), //Top tab button
-                  ),
-                  getVerticalSpace(20),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Header(
+          headerChild: CustomDropDown(),
+        ),
+        getVerticalSpace(15),
 
-                  //this portion contain TextField and Search Button
-                  TicketSearchField(),
-                  getVerticalSpace(20), //20
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                    child: Text(
-                      'Last Visited',
-                      style: kBoldText.copyWith(color: kTextGreenColor),
-                    ),
-                  ),
-                  HistoryAndSearchCard(
-                    cardChild: HistoryAndSearchCardContent(
-                      busName: 'Ena Transport',
-                      busNumber: 'DHK METRO  0001',
-                      isHistory: true,
-                    ),
-                  ),
-                ],
+        //this portion contain TextField and Search Button
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: TabButton(), //Top tab button
+        ),
+        getVerticalSpace(20),
+        Expanded(
+          child: ListView(
+            padding: EdgeInsets.all(0),
+            children: [
+              TicketSearchField(),
+              getVerticalSpace(20), //20
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Text(
+                  'Last Visited',
+                  style: kBoldText.copyWith(color: kTextGreenColor),
+                ),
               ),
-            ),
-          ), //recent visited history are kept in this card
-        ],
-      ),
+              AspectRatio(
+                aspectRatio: 1.71,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.all(0),
+                    itemCount: visitList.length,
+                    itemBuilder: (context, index) {
+                      return HistoryAndSearchCard(
+                        cardChild: HistoryAndSearchCardContent(
+                          isHistory: true,
+                          index: index,
+                        ),
+                      );
+                    }),
+              ),
+            ],
+          ),
+        ), //recent visited history are kept in this card
+      ],
     );
   }
 }
