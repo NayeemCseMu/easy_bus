cd2a971ea171654c4cdcb744ba43e9f247e21cb7
NayeemCseMu <nayeem.cse1@gmail.com>
2020-11-28 10:53:54 +0600

    added fab to remove

--- lib/view/seat/seat_screen.dart
+++ lib/view/seat/seat_screen.dart
@@ -1,7 +1,10 @@
 import 'package:easy_bus/components/app_bar.dart';
+import 'package:easy_bus/provider/tab_item.dart';
+import 'package:easy_bus/utilis/constants.dart';
 import 'package:easy_bus/utilis/size.dart';
 import 'package:easy_bus/view/seat/components/body.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SeatSelect extends StatelessWidget {
   static const String routeName = '/seat_select';
@@ -12,6 +15,18 @@ class SeatSelect extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: buildAppBar(title: 'Sylhet to Dhaka'),
       body: Body(),
+      floatingActionButton: Consumer<HomeTab>(builder: (context, provider, _) {
+        if (provider.seatNumber.isEmpty) {
+          return SizedBox();
+        }
+        return FloatingActionButton(
+          backgroundColor: kPrimaryColor,
+          child: Icon(Icons.clear, size: 30),
+          onPressed: () {
+            provider.clearList();
+          },
+        );
+      }),
     );
   }
 }
